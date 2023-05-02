a6b8207dc08326b175e2204dca1def519358aa0f
kevelscy <kevelscy@gmail.com>
2023-05-02 02:03:17 -0400

    adding rimraf

--- dist/types/components/shadcn-ui/skeleton.d.ts
+++
@@ -1,3 +0,0 @@
-import type { HTMLAttributes } from 'react';
-declare function Skeleton({ className, ...props }: HTMLAttributes<HTMLDivElement>): JSX.Element;
-export { Skeleton };
--- dist/types/lib/hooks/useTheme.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const useTheme: () => {
-    theme: "light" | "dark";
-    toggleTheme: () => void;
-};
